41f9d761cf82c66981858b4137fb5578edf0f74f
Jacob Barnette <jacobcbarnette@gmail.com>
2020-10-19 13:17:41 -0400

    added computer selection logic

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function computerPlay() {
+    const computerChoice = ['rock', 'paper', 'scissors'];
+    let arrayIndex = Math.floor(Math.random() * 3);
+    let computerSelection = computerChoice[arrayIndex];
+    return computerSelection;
+}
+
+
+  
+
+
