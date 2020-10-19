9b6c4b7f5fd95151850a9cca04d3c07fdd535706
Jacob Barnette <jacobcbarnette@gmail.com>
2020-10-19 13:50:40 -0400

    hard coded game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/* computer selection logic */
 function computerPlay() {
     const computerChoice = ['rock', 'paper', 'scissors'];
     let arrayIndex = Math.floor(Math.random() * 3);
@@ -5,7 +6,15 @@ function computerPlay() {
     return computerSelection;
 }
 
+function rockPaperScissors(playerSelection, computerSelection) {
+    playerSelection = 'rock';
+    computerSelection = computerPlay();
 
-  
-
-
+    if(computerSelection === playerSelection) {
+        alert('You have drawn, please try again!');
+    } else if (computerSelection == 'paper') {
+        alert('The computer won! Paper covers rock');
+    } else {
+        alert('You won! Rock beats scissors');
+    }
+}
