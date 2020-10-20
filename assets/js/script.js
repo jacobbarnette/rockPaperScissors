68d8d75acb76f7c8d81d2dd71e009b3c3023405d
Jacob Barnette <jacobcbarnette@gmail.com>
2020-10-20 19:22:26 -0400

    added fluid gmae logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,31 @@ function computerPlay() {
 }
 
 function rockPaperScissors(playerSelection, computerSelection) {
-    playerSelection = 'rock';
+    playerSelection = prompt('Rock, Paper, or Scissors');
+    playerSelection = playerSelection.toLowerCase();
     computerSelection = computerPlay();
 
-    if(computerSelection === playerSelection) {
-        alert('You have drawn, please try again!');
-    } else if (computerSelection == 'paper') {
-        alert('The computer won! Paper covers rock');
-    } else {
-        alert('You won! Rock beats scissors');
+    if(playerSelection === computerSelection) {
+        alert('The round has ended in a draw');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert('Congrats! You won! Rock beats paper');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        alert('Sorry, you lost this round! Paper covers rock');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        alert('Congrats! You won! Paper covers rock');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        alert('Sorry, you lost this round! Scissors cut paper');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert('Congrats! You won! Scissors cuts paper');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        alert('Sorry, you lost this round! Rock beats scissors');
     }
+}
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        rockPaperScissors();
+        console.log(i);
+    }
+    console.log('You reached your turn limit!')
 }
