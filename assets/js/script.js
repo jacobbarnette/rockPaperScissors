f8439ba3460211a26517376d3bfc5b73cadc7364
Jacob Barnette <jacobcbarnette@gmail.com>
2020-10-20 19:42:49 -0400

    change p tag to proper id computerScoreCount

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+let draw = 0;
+let playerScore = 0;
+let computerScore = 0;
+let drawCount = document.getElementById('drawCount');
+let playerScoreCount = document.getElementById('playerScoreCount');
+let computerScoreCount = document.getElementById('computerScoreCount');
+
+
+
+
 /* computer selection logic */
 function computerPlay() {
     const computerChoice = ['rock', 'paper', 'scissors'];
@@ -6,6 +16,7 @@ function computerPlay() {
     return computerSelection;
 }
 
+/* rock paper scissors logic */
 function rockPaperScissors(playerSelection, computerSelection) {
     playerSelection = prompt('Rock, Paper, or Scissors');
     playerSelection = playerSelection.toLowerCase();
@@ -13,21 +24,36 @@ function rockPaperScissors(playerSelection, computerSelection) {
 
     if(playerSelection === computerSelection) {
         alert('The round has ended in a draw');
+        draw++;
+        drawCount.textContent = `Games Drawn: ${draw}`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         alert('Congrats! You won! Rock beats paper');
+        playerScore++;
+        playerScoreCount.textContent = `Player Rounds won: ${playerScore}`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         alert('Sorry, you lost this round! Paper covers rock');
+        computerScore++;
+        computerScoreCount.textContent = `Computer Rounds won: ${computerScore}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         alert('Congrats! You won! Paper covers rock');
+        playerScore++;
+        playerScoreCount.textContent = `Player Rounds won: ${playerScore}`;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         alert('Sorry, you lost this round! Scissors cut paper');
+        computerScore++;
+        computerScoreCount.textContent = `Computer Rounds won: ${computerScore}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         alert('Congrats! You won! Scissors cuts paper');
+        playerScore++;
+        playerScoreCount.textContent = `Player Rounds won: ${playerScore}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         alert('Sorry, you lost this round! Rock beats scissors');
+        computerScore++;
+        computerScoreCount.textContent = `Computer Rounds won: ${computerScore}`;
     }
 }
 
+/* run game to turn limit */
 function game() {
     for(let i = 0; i < 5; i++) {
         rockPaperScissors();
